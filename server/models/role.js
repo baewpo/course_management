33ecef0f3a8dc0391f81a9ec3978ee8f5f2191ac
suphatsorn <praewsps1334@gmail.com>
2025-01-11 08:25:16 +0700

    fix: handle db schema

--- server/models/role.js
+++ server/models/role.js
@@ -1,18 +1,23 @@
 const { DataTypes } = require('sequelize');
 const { sequelize } = require('../config/db');
 
-const Role = sequelize.define('Role', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  name: {
-    type: DataTypes.STRING
-  }
-}, {
-  tableName: 'roles',
-  timestamps: false,
-});
+const Role = sequelize.define(
+	"Role",
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		name: {
+			type: DataTypes.STRING,
+		},
+	},
+	{
+		tableName: "roles",
+		timestamps: false,
+		schema: "course",
+	}
+)
 
 module.exports = Role;
